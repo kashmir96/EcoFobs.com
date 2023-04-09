4a527bdbf6f7b8e7e473ce0717940ad2b7fc218c
Curtis Fairweather <curtis@oso.nz>
2023-04-10 11:46:47 +1200

    updating pixel across site

--- content/pages/confirmation/_index.md
+++ content/pages/confirmation/_index.md
@@ -3,10 +3,6 @@ title: "Confirmation"
 js: /js/confirmation.js
 ---
 {{<rawhtml>}}
-<!-- Facebook Pixel -->
-<img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=576633227759446&ev=PageView&noscript=1"
-    />
 
 <div class="order-confirmation">
       <h1>Thank you for your order!</h1>
--- static/js/confirmation.js
+++ static/js/confirmation.js
@@ -10,19 +10,6 @@
 
 //!-- End TikTok Pixel Code -->
 
-//!-- Meta Pixel Code -->
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '576633227759446');
-    fbq('track', 'PageView');
-//!-- End Meta Pixel Code -->
-
 //!-- Google Tracking Tags -->
 // Google tag (gtag.js)
 var gtagScript = document.createElement('script');
